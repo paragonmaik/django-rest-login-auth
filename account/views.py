159653ce80771f78085448bb46681b15d8ece9bd
Maikson <estevan.maik@gmail.com>
2023-04-29 11:19:49 -0300

    chore: created UserRegistrationView

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class UserRegistrationView(APIView):
+    """
+    placeholder docstring
+    """
+
+    def register(self, request, format=None):
+        return Response({"message": "test"}, status=status.HTTP_201_CREATED)
