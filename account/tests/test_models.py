eaced60cbe2d5f3d81729d33ee9fa61bf604903c
Maikson <estevan.maik@gmail.com>
2023-05-01 14:13:43 -0300

    chore: added docstrings

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-from account.models import UserManager, User
+from account.models import User
 
 
 class TestModels(TestCase):
+    """
+    Test models class
+    """
 
     def setUp(self):
         self.user1 = User.objects.create(
@@ -13,5 +16,8 @@ class TestModels(TestCase):
         )
 
     def test_user_is_created(self):
+        """
+        Tests if user is created and is saved to the database
+        """
         created_user = User.objects.get(email="teste@email.com")
         self.assertEqual(self.user1.email, created_user.email)
