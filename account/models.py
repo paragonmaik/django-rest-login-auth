e5da8302f96cb397f53ccf703346f08fdfe4e0a6
Maikson <estevan.maik@gmail.com>
2023-05-04 14:35:27 -0300

    refactor: removes unused imports

--- account/models.py
+++ account/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
-from django.contrib.auth.hashers import make_password, check_password
 
 
 class UserManager(BaseUserManager):
