bfe0045219b53a05dc5df3eb59a3f8d8b27862b0
Maikson <estevan.maik@gmail.com>
2023-05-04 14:34:30 -0300

    feat: handles exception in case of missing data

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -1,6 +1,6 @@
+import json
 from django.test import TestCase, Client
 from django.urls import reverse
-import json
 
 
 class TestViews(TestCase):
@@ -26,7 +26,24 @@ class TestViews(TestCase):
             "password2": "Teste123@@",
             "terms_conditions": "True"
         })
+
         response_body = json.loads(response.content.decode("utf-8"))
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response_body["message"], "Registered!")
         self.assertTrue("token" in response_body)
+
+    def test_unsuccessful_user_register_post(self):
+        """
+        Test if user can be registered successfully
+        """
+        response = self.client.post(self.register_url, {
+            "name": "",
+            "email": "email@example.com",
+            "password": "Teste123@@",
+            "password2": "Teste123@@",
+            "terms_conditions": "True"
+        })
+
+        response_body = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response.status_code, 422)
+        self.assertTrue("errors" in response_body)
--- account/views.py
+++ account/views.py
@@ -1,26 +1,36 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from account.serializers import UserRegistrationSerializer
+from .renderers import UserRenderer
 
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
     return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
     }
 
 
 class UserRegistrationView(APIView):
     """
-    User Registration class with a single post method
+    User Registration class with a post method
     """
+    serializer_class = UserRegistrationSerializer
+    renderer_classes = (UserRenderer,)
 
     def post(self, request, format=None):
-        serializer = UserRegistrationSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        """
+        POST method that registers the user
+        """
+        serializer = self.serializer_class(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except serializers.ValidationError:
+            return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
         user = serializer.save()
         token = get_tokens_for_user(user)
         return Response({"token": token, "message": "Registered!"},
