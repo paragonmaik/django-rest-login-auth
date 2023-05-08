8124cfe2ce675f410397e71aff6d6f328f913180
Maikson <estevan.maik@gmail.com>
2023-05-08 17:13:59 -0300

    chore: added module and class docstrings

--- account/serializers.py
+++ account/serializers.py
@@ -1,21 +1,31 @@
+"""
+Module for users data serialization.
+"""
 from rest_framework import serializers
 from account.models import User
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    """
+    Serializes user registration data.
+    ...
+    Methods:
+        validate(attrs):
+            Validates if password and password2 fields are a match.
+    """
     password2 = serializers.CharField(
-        style={'input_type': 'password'}, write_only=True)
+        style={"input_type": "password"}, write_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'name', 'password', 'password2', 'terms_conditions']
+        fields = ["email", "name", "password", "password2", "terms_conditions"]
         extra_kwargs = {
-            'password': {'write_only': True}
+            "password": {"write_only": True}
         }
 
     def validate(self, attrs):
-        password = attrs.get('password')
-        password2 = attrs.get('password2')
+        password = attrs.get("password")
+        password2 = attrs.get("password2")
         if password != password2:
             raise serializers.ValidationError(
                 "Passwords do not match!")
