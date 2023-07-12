22c76f74d2d00b20c735d671d0aa5f55d62f2f8e
Maikson <estevan.maik@gmail.com>
2023-07-12 18:13:55 -0300

    feat: added admin registration url

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,10 @@
 Module that holds account app URLs.
 """
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from .views import UserRegistrationView, UserLoginView, AdminRegistrationView
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
+    path("register/admin/", AdminRegistrationView.as_view(), name="register_admin"),
     path("login/", UserLoginView.as_view(), name="login"),
 ]
