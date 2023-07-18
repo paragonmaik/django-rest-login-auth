31221a1edebe368ef244577afaecaf0f56a5a870
Maikson <estevan.maik@gmail.com>
2023-07-18 19:04:05 -0300

    chore: added send_reset_password url

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,14 @@
 Module that holds account app URLs.
 """
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserPasswordChangeView
+from .views import (UserRegistrationView, UserLoginView,
+                    UserPasswordChangeView, SendPasswordResetEmailView)
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("password-change/", UserPasswordChangeView.as_view(),
          name="password_change"),
+    path("send-reset-password-email/", SendPasswordResetEmailView.as_view(),
+         name="send_reset_password_email")
 ]
