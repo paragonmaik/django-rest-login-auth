020174481a16ae9e831dcf30eb4ea4211ca6f145
Maikson <estevan.maik@gmail.com>
2023-07-18 18:06:46 -0300

    chore: updated secret key config

--- book/settings.py
+++ book/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+7q$5#15p5vm@*fjv_l4%hc-otb(a0vsbxq0#&9$f4^jg_^&s'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
